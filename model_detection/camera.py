bc59825b6f638e425af6235439e7310a08dae3bf
Serg <shaytanbat@gmail.com>
2021-03-26 21:15:00 +0200

    Changed camera.py

--- model_detection/camera.py
+++ model_detection/camera.py
@@ -4,15 +4,16 @@ import os
 # import numpy as np
 from django.conf import settings
 
-# load our serialized bird detector model from disk
-bird_detection_webcam = cv2.CascadeClassifier(os.path.join(settings.BASE_DIR, 'opencv_haarcascade_data/bird_cascade.xml'))
 
+# load our serialized bird detector model from disk
+bird_detection_webcam = cv2.CascadeClassifier(os.path.join(
+			settings.BASE_DIR, 'opencv_haarcascade_data/bird_cascade.xml'))
 
 class VideoCamera(object):
 	def __init__(self):
-		self.video = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # 1 - for USB webcam on the laptop, 0 - for integrated laptop's webcam
+		self.video = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # 1 - for USB webcam on the laptop
 		if not self.video.isOpened():
-			self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+			self.video = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 0 - for integrated laptop's webcam
 
 	def __del__(self):
 		self.video.release()
