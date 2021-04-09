d95aa8563d0bcaed301da55ba0ab2f6ebdb6a64b
AlexNyzovyi <alex@ginkgoukraine.com>
2021-04-09 16:30:42 +0300

    `asgi.py`: changed imports order (AuthMiddlewareStack needs to Django has been initialized)

--- KKBird/asgi.py
+++ KKBird/asgi.py
@@ -10,16 +10,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from django.urls import re_path
-
-from model_detection.views import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KKBird.settings')
 
 django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.urls import re_path
+
+
+from model_detection.views import ChatConsumer
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
