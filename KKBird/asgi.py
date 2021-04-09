40b1c090ab9ec04f74e017b693a630f23f58893c
AlexNyzovyi <alex@ginkgoukraine.com>
2021-04-09 16:26:54 +0300

    Added Django setup into the asgi.py

--- KKBird/asgi.py
+++ KKBird/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
@@ -18,6 +19,7 @@ from model_detection.views import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KKBird.settings')
 
+django.setup()
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
